cb689dd64c448c1723dc29b64f79a67abeae717d
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2015-10-31 00:27:13 +0100

    Viewpager til fragmenter med øvelser

--- vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/Frag1VælgEmneOgNiveau.java
+++ vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/Frag1VælgEmneOgNiveau.java
@@ -9,25 +9,26 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import dk.lundogbendsen.vuc.domæne.Logik;
 
 
-public class VælgEmneOgNiveauFrag extends Fragment implements AbsListView.OnItemClickListener {
+public class Frag1VælgEmneOgNiveau extends Fragment implements AbsListView.OnItemClickListener {
 
-    private AbsListView listView;
+    private ListView listView;
 
     private ListAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.vaelg_emne_og_niveau_frag, container, false);
+        View view = inflater.inflate(R.layout.frag1_vaelg_emne_og_niveau, container, false);
 
         adapter = new ArrayAdapter(getActivity(),
                 android.R.layout.simple_list_item_1, android.R.id.text1, Logik.i.bru.fag.emner);
 
-        listView = (AbsListView) view.findViewById(android.R.id.list);
+        listView = (ListView) view.findViewById(android.R.id.list);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(this);
@@ -38,7 +39,8 @@ public class VælgEmneOgNiveauFrag extends Fragment implements AbsListView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//        getFragmentManager().beginTransaction().replace(R.id.hovedakt_indhold, )
+        getFragmentManager().beginTransaction()
+                .replace(R.id.hovedakt_indhold, new Frag2ØvelseViewpager()).addToBackStack(null).commit();
     }
 
 }
--- vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/HovedAkt.java
+++ vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/HovedAkt.java
@@ -3,6 +3,7 @@ package dk.lundogbendsen.vuc;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,7 +11,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -53,7 +53,7 @@ public class HovedAkt extends AppCompatActivity
 
         if (savedInstanceState==null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.hovedakt_indhold, new VælgEmneOgNiveauFrag()).commit();
+                    .add(R.id.hovedakt_indhold, new Frag1VælgEmneOgNiveau()).commit();
         }
 
     }
@@ -68,36 +68,15 @@ public class HovedAkt extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.hoved_akt, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
+        if (id == R.id.emner) {
+            getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+//            getSupportFragmentManager().popBackStack();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
