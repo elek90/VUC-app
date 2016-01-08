e00bb57347e0998527e86d9cf3808f8b9fc5f4bd
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2016-01-08 21:22:25 +0100

    Flere emner

--- vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/domæne/Logik.java
+++ vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/domæne/Logik.java
@@ -36,48 +36,95 @@ public class Logik {
     Hold naturfag = hans.holdListe[0];
     naturfag.emner = new Emne[]{
             new Emne("Lys - Niveau G"),
+            new Emne("Vejr"),
+            new Emne("Kemi i din hverdag"),
             new Emne("Lys - Niveau F-E-D"),
             new Emne("Husholdningskemi - Niveau G"),
             new Emne("Syrer og baser"),
     };
-// Opgave -> Trin
-    // OpgaveKategori -> TrinOverskrift
+
     naturfag.emner[0].trin = new Trin[]{
-      /* VO */      new Trin("VIDEN OM", tavle),
-      /* VO */      new Trin("Bliv klog på lys", bog, "Øjet kan se forskellige farver. De forskellige farver skyldes, at lyset kan have forskellige bølgelængder. \nBølgelængden af synligt lys ligger i intervallet fra ca. 380 nm (violet lys) til ca. 740 nm (rødt lys). De mellemliggende bølgelængder ses som blå, grøn, gul og orange. \n\nMere kortbølget stråling kaldes ultraviolet lys eller UV, og mere langbølget stråling kaldes infrarødt lys eller IR. I huden findes varmefølsomme receptorer som omdanner IR til varme. I huden findes endvidere pigment som aktiveres når huden udsættes for UV. Nogle dyr, f.eks. bier, kan se UV, mens andre dyr, f.eks. klapperslanger, kan se IR.\n\nLæs mere på https://da.wikipedia.org/wiki/Lys"),
-      /* UD */      new Trin("UDSTYR", værktøj),
-      /* UD */      new Trin("Optag et spektrum med et spektrometer", video, "https://www.youtube.com/watch?v=ZWZYtfT0ZZk&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
-      /* UD */      new Trin("Lav dit eget spektroskop", video, "https://www.youtube.com/watch?v=Bke9CE59c-A&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
-      /* UD */      new Trin("Brug af hjemmelavet spektroskop", video, "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
-      /* FO */      new Trin("FORSØG", pen_og_blyant),
-      /* FO */      new Trin("Tag et billede af den blå himmel", foto),
-      /* FO */      new Trin("Tag et billede af en elektrisk pære", foto),
-      /* FO */      new Trin("Tag billeder af spektrum", foto),
-      /* FO */      new Trin("Tag billeder af lyskilde", foto),
-      /* FO */      new Trin("Beskriv dine observationer", notesblok),
-            new Aflevering("Send rapport"),
+      new Trin(tavle, "VIDEN OM"),
+      new Trin(bog, "Bliv klog på lys", "Øjet kan se forskellige farver. De forskellige farver skyldes, at lyset kan have forskellige bølgelængder. \nBølgelængden af synligt lys ligger i intervallet fra ca. 380 nm (violet lys) til ca. 740 nm (rødt lys). De mellemliggende bølgelængder ses som blå, grøn, gul og orange. \n\nMere kortbølget stråling kaldes ultraviolet lys eller UV, og mere langbølget stråling kaldes infrarødt lys eller IR. I huden findes varmefølsomme receptorer som omdanner IR til varme. I huden findes endvidere pigment som aktiveres når huden udsættes for UV. Nogle dyr, f.eks. bier, kan se UV, mens andre dyr, f.eks. klapperslanger, kan se IR.\n\nLæs mere på https://da.wikipedia.org/wiki/Lys"),
+      new Trin(værktøj, "UDSTYR"),
+      new Trin(video, "Optag et spektrum med et spektrometer", "https://www.youtube.com/watch?v=ZWZYtfT0ZZk&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
+      new Trin(video, "Lav dit eget spektroskop", "https://www.youtube.com/watch?v=Bke9CE59c-A&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
+      new Trin(video, "Brug af hjemmelavet spektroskop", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(pen_og_blyant, "FORSØG"),
+      new Trin(foto, "Tag et billede af den blå himmel"),
+      new Trin(foto, "Tag et billede af en elektrisk pære"),
+      new Trin(foto, "Tag billeder af spektrum"),
+      new Trin(foto, "Tag billeder af lyskilde"),
+      new Trin(notesblok, "Beskriv dine observationer"),
+      new Aflevering("Send rapport"),
     };
 
     naturfag.emner[1].trin = new Trin[]{
-            new Trin("Bliv klog på lys", bog, "Øjet kan se forskellige farver. De forskellige farver skyldes, at lyset kan have forskellige bølgelængder.\n\nLæs mere på https://da.wikipedia.org/wiki/Lys"),
-            new Trin("Sådan bruger du udstyret", bog, "Et spektrometer kan skille de forskellige farver ad. Kig i det, mens du holder det op en lyskilde, f.eks. himlen."),
-            new Trin("Optag et spektrum med et spektrometer", bog, "https://www.youtube.com/watch?v=ZWZYtfT0ZZk&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
-            new Trin("Gennemfør din egen undersøgelse", værktøj, "Nu skal du bruge spektrometret"),
-            new Trin("Tag et billede af den blå himmel", foto),
-            new Trin("Tag billeder af lyskilde", foto),
-            new Aflevering("Send rapport"),
+      new Trin(tavle, "VIDEN OM"),
+      new Trin(video, "Bliv klog på vejret. Kort præsentation af forskellige vejrfænomener.", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Stormvejr – kraftig blæst – vindstille som kontrast – lavtryk og højtryk", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Nedbør: regn – sne – hagl", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Temperaturer (ekstreme) – frost – hedebølge", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Skytyper", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(værktøj, "UDSTYR"),
+      new Trin(video, "Lav din egen vindmåler (alternativt udlånes Vaavud vindmåler til jackstik)", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Lav din egen regnmåler", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Brug din telefon som lysmåler – hente app.", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(video, "Brug din telefon som termometer – hente app. (alternativt udlånes Thermodo termometer til jackstik)", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+      new Trin(pen_og_blyant, "FORSØG"),
+      new Trin(foto, "Undersøge vind: Hvordan opstår vind?"),
+      new Trin(foto, "Måle vindhastigheder i naturen."),
+      new Trin(foto, "Undersøge nedbør: Hvordan opstår nedbør?"),
+      new Trin(foto, "Undersøge skyer – lys (solindstråling) – temparetur"),
+      new Trin(foto, "Tag billeder af skytyper"),
+      new Aflevering("Send rapport"),
     };
 
     naturfag.emner[2].trin = new Trin[]{
-            new Trin("Kemi i køkkenet", bog),
-            new Trin("Forberedelse", værktøj),
-            new Trin("Mel og vand", bog),
-            new Trin("Gennemfør din egen undersøgelse", værktøj),
-            new Trin("Tag billeder af blanding", foto),
+            new Trin(tavle, "Bliv klog på kemi i hjemmet"),
+            new Trin(video, "Syrers og basers egenskaber", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(video, "pH skala og måling af syrers og basers styrke. Sammenhæng mellem fortynding og styrke", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(video, "Forklaringer på faglige begreber." +
+                    "Syrer indeholder H+-ioner.\n" +
+                    "Baser indeholder OH-- ioner.\n" +
+                    "Hvad er et neutralt stof?", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(video, "Hvad er ioner? – grundstoffernes periodiske system og oktetreglen.", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(video, "Sikkerhedsmærkning af kemikalier", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(værktøj, "UDSTYR"),
+            new Trin(video, "Fremstille egne indikator farvestoffer ud fra forskellige planter", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(video, "Fremstille eget indikatorpapir.", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(pen_og_blyant, "FORSØG"),
+            new Trin(video, "Sikkerhedsinstruktioner. Det er vigtigt at du bruger briller og handsker", "https://youtu.be/n7tt0I0P8gY?list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi"),
+            new Trin(foto, "Kemikalier, der bruges til rengøring i hjemmet. " +
+                    "Tag flere billeder. Der er både faste og flydende, og inddele i syrer-baser-neutrale stoffer. \n" +
+                    "Husk at kommentere resultaterne."),
+            new Trin(foto, "Undersøge madvarer og drikkevarer - både faste og flydende, og inddele i syrer-baser-neutrale stoffer. \n" +
+                    "Kommentere resultaterne."),
             new Aflevering("Send rapport"),
     };
 
+
+
     naturfag.emner[3].trin = new Trin[]{
+            new Trin(bog, "Bliv klog på lys", "Øjet kan se forskellige farver. De forskellige farver skyldes, at lyset kan have forskellige bølgelængder.\n\nLæs mere på https://da.wikipedia.org/wiki/Lys"),
+            new Trin(bog, "Sådan bruger du udstyret", "Et spektrometer kan skille de forskellige farver ad. Kig i det, mens du holder det op en lyskilde, f.eks. himlen."),
+            new Trin(bog, "Optag et spektrum med et spektrometer", "https://www.youtube.com/watch?v=ZWZYtfT0ZZk&list=PLW_WKZMCQZMvbVXJ8yHLCGNx8LJJD5WAi&index=2"),
+            new Trin(værktøj, "Gennemfør din egen undersøgelse", "Nu skal du bruge spektrometret"),
+            new Trin(foto, "Tag et billede af den blå himmel"),
+            new Trin(foto, "Tag billeder af lyskilde"),
+            new Aflevering("Send rapport"),
+    };
+
+    naturfag.emner[4].trin = new Trin[]{
+            new Trin(bog, "Kemi i køkkenet"),
+            new Trin(værktøj, "Forberedelse"),
+            new Trin(bog, "Mel og vand"),
+            new Trin(værktøj, "Gennemfør din egen undersøgelse"),
+            new Trin(foto, "Tag billeder af blanding"),
+            new Aflevering("Send rapport"),
+    };
+
+    naturfag.emner[5].trin = new Trin[]{
             new Trin("Om PH-værdi"),
             new Trin("Sikkerhed"),
             new Trin("Mål PH-værdien af saltsyre"),
--- vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/domæne/Trin.java
+++ vucAndroid/app/src/main/java/dk/lundogbendsen/vuc/domæne/Trin.java
@@ -4,6 +4,8 @@ import android.net.Uri;
 
 import java.io.Serializable;
 
+import dk.lundogbendsen.vuc.diverse.Log;
+
 /**
  * Created by j on 01-11-15.
  */
@@ -22,26 +24,38 @@ public class Trin implements Serializable {
   }
 
   public Trin(String navn) {
-    this(navn, Ikon.bog);
+    this(Ikon.bog, navn);
   }
 
-  public Trin(String navn, Ikon ikon) {
+  public Trin(Ikon ikon, String navn) {
     this.navn = navn;
     this.ikon = ikon;
+    splitEvtNavn();
   }
 
-  public Trin(String navn, Ikon ikon, String ekstra) {
-    this(navn, ikon);
+  public Trin(Ikon ikon, String navn, String ekstra) {
+    this(ikon, navn);
     if (ekstra.startsWith("https://youtu.be")) {
       videoUrl =  Uri.parse(ekstra).getPath().substring(1);
-      tekst = "\n(Klik på videoen ovenfor)";
+      if (tekst==null) tekst = "(video)";
     }
     else if (ekstra.startsWith("https://www.youtube.com")) {
       videoUrl =  Uri.parse(ekstra).getQueryParameter("v");
-      tekst = "\n(Klik på videoen ovenfor)";
+      if (tekst==null) tekst = "(video)";
     }
     //else if (ekstra.startsWith("http")) url = ekstra;
     else tekst = ekstra;
+  }
+
+  private void splitEvtNavn() {
+    int pos = navn.indexOf(".");
+    if (pos<0 && navn.length()>16) pos = navn.indexOf(":");
+    if (pos<0 && navn.length()>16) pos = navn.indexOf("(");
+    if (pos<0 && navn.length()>16) pos = navn.indexOf("-");
+    if (pos>0) {
+      tekst = navn;
+      navn = navn.substring(0, pos).trim();
+    }
   }
 
   @Override
